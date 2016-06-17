ca85249461f87c996c79c5853e3741f85d52a4be
Palak Jain <palakjain024@gmail.com>
2016-06-17 14:37:37 +0800

    working file for 1.5C to C

--- src/input_pkg.vhd
+++ src/input_pkg.vhd
@@ -34,7 +34,7 @@ package input_pkg is
   constant C_star :  sfixed(0 downto -35) := to_sfixed(0.0001, 0, -35);
   constant theta_L_star : sfixed(15 downto -16) := to_sfixed(200, 15, -16);
   constant theta_C_star : sfixed(15 downto -16):= to_sfixed(10000, 15, -16);
-  constant theta_C_init : sfixed(15 downto -16):= to_sfixed(20000, 15, -16);
+  constant theta_C_init : sfixed(15 downto -16):= to_sfixed(6667, 15, -16);
   -- Adaptive Gain for theta correction
   constant e11 : sfixed(24 downto -10) := to_sfixed(-0.1,24,-10);
   constant e22 : sfixed(24 downto -10) := to_sfixed(-5e5,24,-10);
