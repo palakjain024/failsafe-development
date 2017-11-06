9b24980f1a60518f17ead51520f4eb8932e503f2
Palak Jain <palakjain024@gmail.com>
2017-11-06 20:02:11 +0800

    commit

--- src/processor_core.vhd
+++ src/processor_core.vhd
@@ -27,33 +27,30 @@ architecture Behavioral of processor_core is
 
 ---- Component definition ----
  -- ILA core
-    COMPONENT ila_0
-
-PORT (
-    clk : IN STD_LOGIC;
-
-
-    trig_in : IN STD_LOGIC;
-    trig_in_ack : OUT STD_LOGIC;
-    probe0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    probe1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    probe2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    probe3 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    probe4 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    probe5 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-    probe6 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-    probe7 : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
-);
-END COMPONENT  ;
+COMPONENT ila_0
+ 
+ PORT (
+     clk : IN STD_LOGIC;
+ 
+ 
+ 
+     probe0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
+     probe1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
+     probe2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
+     probe3 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
+     probe4 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+     probe5 : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
+ );
+ END COMPONENT  ;
  -- Converter estimator
  component plant_x
  port (   Clk : in STD_LOGIC;
           Start : in STD_LOGIC;
-          Mode : in INTEGER range 1 to 4;
+          Mode : in INTEGER range 1 to 2;
           load : in sfixed(n_left downto n_right);
-          plt_y : in vect2;
+          plt_x : in vect2;
           done : out STD_LOGIC := '0';
-          FD_residual : out sfixed(n_left downto n_right) := to_sfixed(0, n_left, n_right);
+          FD_residual : out vect2 := to_sfixed(0, n_left, n_right);
           plt_z : out vect2 := (to_sfixed(0,n_left,n_right),to_sfixed(0,n_left,n_right))
          );
  end component plant_x;
@@ -62,11 +59,12 @@ END COMPONENT  ;
 ---- Signal definition for components ----
 
 -- ILA core
- signal trig_in_ack, trig_in : STD_LOGIC := '0';
- signal probe_normfd, probe_normc, probe_z1fd, probe_z2fd, probe_y1c : STD_LOGIC_VECTOR(31 downto 0);
- signal probe_y2c, probe_y1p, probe_y2p : STD_LOGIC_VECTOR(31 downto 0);
+ -- signal trig_in_ack, trig_in : STD_LOGIC := '0';
+ signal probe_thetaL, probe_thetaC : STD_LOGIC_VECTOR(31 downto 0);
+ signal probe_x1, probe_x2 : STD_LOGIC_VECTOR(31 downto 0);
+ signal probe_z1, probe_z2 : STD_LOGIC_VECTOR(31 downto 0);
  
- -- General
+  -- General
  signal counter : integer range 0 to 50000 := -1;
  
  -- Common Inputs 
@@ -76,7 +74,7 @@ END COMPONENT  ;
  -- Plant outputs and Fault detection logic
  signal done: STD_LOGIC := '1';
  signal z_val: vect2 := (to_sfixed(0,n_left,n_right),to_sfixed(0,n_left,n_right));
- signal FD_residual : sfixed(n_left downto n_right) := to_sfixed(0,n_left,n_right);
+ signal FD_residual : vect2 := (to_sfixed(0,n_left,n_right),to_sfixed(0,n_left,n_right));
  signal flag : STD_LOGIC := '0';
  
 begin
@@ -87,7 +85,7 @@ Clk => clk,
 Start => start,
 Mode => mode,
 load => load,
-plt_y => pc_y,
+plt_x => pc_y,
 Done => done,
 FD_residual => FD_residual,
 plt_z => z_val
@@ -96,17 +94,13 @@ plt_z => z_val
 ila_inst_1: ila_0
 PORT MAP (
     clk => clk_ila,
-    
-    trig_in => trig_in,
-    trig_in_ack => trig_in_ack,
-    probe0 => probe_normfd, 
-    probe1 => probe_normc, 
-    probe2 => probe_z1fd,  
-    probe3 => probe_z2fd, 
-    probe4 => probe_y1c,
-    probe5 => probe_y2c,
-    probe6 => probe_y1p,
-    probe7 => probe_y2p
+
+    probe0 => probe_thetaL, 
+    probe1 => probe_thetaC, 
+    probe2 => probe_x1, 
+    probe3 => probe_x2, 
+    probe4 => probe_z1,
+    probe5 => probe_z2
     
 ); 
 ---- Processes ----
@@ -123,14 +117,12 @@ CoreLOOP: process(clk, pc_pwm, pc_en)
 
 
    ---- ILA ----
-      probe_normfd <= result_type(FD_residual);
-      probe_normc <= result_type(norm_c); 
-      probe_z1fd  <= result_type(z_val(0));
-      probe_y1p <= result_type(pc_y(0)) ; 
-      probe_y2p <= result_type(pc_y(1));
-      probe_z2fd <= result_type(z_val(1));
-      probe_y1c <= result_type(y_est_c(0));
-      probe_y2c <= result_type(y_est_c(1));
+      probe_thetaL  <= result_type(z_val(0));
+      probe_thetaC <= result_type(pc_y(0)) ; 
+      probe_x1 <= result_type(pc_y(1));
+      probe_x2 <= result_type(z_val(1));
+      probe_z1 <= result_type(y_est_c(0));
+      probe_z2 <= result_type(y_est_c(1));
            
           ---- Output to main ----
           
@@ -142,34 +134,27 @@ CoreLOOP: process(clk, pc_pwm, pc_en)
                       
            ---- To determine Mode PWM for top switch ----
                if counter = 0 then
-                       if (pc_pwm(0) = '1' and pc_pwm(1) = '0' ) then
-                       -- Mode SW1 top conducting
-                         mode <= 1;
-                       elsif (pc_pwm(0) = '0' and pc_pwm(1) = '0' ) then
-                       -- Mode no top SW conducting
-                         mode <= 2; 
-                       elsif (pc_pwm(0) = '0' and pc_pwm(1) = '1' ) then
-                       -- Mode SW2 top conducting
-                         mode <= 3;
-                       elsif (pc_pwm(0) = '1' and pc_pwm(1) = '1' ) then
-                       -- Mode both top SW conducting
-                         mode <= 4;  
+                       if (pc_pwm(0) = '0') then
+                       -- SW2 Bottom switch conducting
+                         mode <= 2;
+                       else
+                       -- SW1 top Switch conducting
+                         mode <= 1;  
                        end if;
-              end if;
+               end if;
            ---- For constant time step 500 ns Matrix Mutiplication to run ----
                 if (counter = 1) then
                   start <= '1';
-                  trig_in <= '1'; 
-                  elsif (counter = 2) then
+                elsif (counter = 2) then
                   start <= '0';
-                  else null;
+                else null;
                 end if; 
                  
-                 if (counter = 49) then
+                if (counter = 49) then
                     counter <= 0;
-                    else
+                else
                     counter <= counter + 1;
-                 end if;
+                end if;
                            
     end if; -- pc_en
    end if; -- Clk
